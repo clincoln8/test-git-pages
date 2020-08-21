bb4e7769f3be1821bc397d691ea3451b36e46417
Christie Lincoln <clincoln@ucsd.edu>
2020-08-21 10:56:21 -0700

    Updates

---
+++ precache-manifest.a6f33d813a09d550ed3139a036652380.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f705edbc8d6107e8faca16e07800cad1",
+    "url": "/test-git-pages/index.html"
+  },
+  {
+    "revision": "e39157524609f865c569",
+    "url": "/test-git-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "01b7b4589db6b29282fd",
+    "url": "/test-git-pages/static/js/2.6ba974bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-git-pages/static/js/2.6ba974bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e39157524609f865c569",
+    "url": "/test-git-pages/static/js/main.a91d2cb4.chunk.js"
+  },
+  {
+    "revision": "85225c6eb7209dcfa54c",
+    "url": "/test-git-pages/static/js/runtime-main.ac5d3ba4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/test-git-pages/react-browser/precache-manifest.fb36b85a6b9d92fd2e1a126b5a96d174.js"
+  "/test-git-pages/precache-manifest.a6f33d813a09d550ed3139a036652380.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test-git-pages/react-browser/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test-git-pages/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
