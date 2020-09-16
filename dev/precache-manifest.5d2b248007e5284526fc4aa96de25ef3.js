db2e2e74f376264b4b95d5d73a088ccb31cbbed5
Christie Lincoln <clincoln@ucsd.edu>
2020-09-15 17:52:50 -0700

    add dev browser

---
+++ dev/precache-manifest.5d2b248007e5284526fc4aa96de25ef3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ece53866ce997e693408cae42003df4",
+    "url": "/test-git-pages/react-browser/build/index.html"
+  },
+  {
+    "revision": "ba2c1bddc59a83f12c23",
+    "url": "/test-git-pages/react-browser/build/static/css/main.0d1858dd.chunk.css"
+  },
+  {
+    "revision": "161badd83d898aeb493a",
+    "url": "/test-git-pages/react-browser/build/static/js/2.065765b6.chunk.js"
+  },
+  {
+    "revision": "1bcefd02b7d05499a818042383aa8e83",
+    "url": "/test-git-pages/react-browser/build/static/js/2.065765b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba2c1bddc59a83f12c23",
+    "url": "/test-git-pages/react-browser/build/static/js/main.9bc576f3.chunk.js"
+  },
+  {
+    "revision": "e9136411ac77b5deb27e",
+    "url": "/test-git-pages/react-browser/build/static/js/runtime-main.f63e1d32.js"
+  }
+]);
