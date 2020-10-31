df6717390ec7236e671a66fa6274c2c1256e960e
Christie Lincoln <clincoln@ucsd.edu>
2020-10-30 22:47:00 -0700

    backend and front end updates

---
+++ dev/build/precache-manifest.8e5cca0ddc61d51487aee5ef7b843334.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84335a9b1d1b93e4b337988c582b1e42",
+    "url": "/test-git-pages/dev/index.html"
+  },
+  {
+    "revision": "1ee8a3c4087589bf1544",
+    "url": "/test-git-pages/dev/static/css/main.abef12e3.chunk.css"
+  },
+  {
+    "revision": "cfdff34ba9b9b829bf89",
+    "url": "/test-git-pages/dev/static/js/2.3dd218b7.chunk.js"
+  },
+  {
+    "revision": "1bcefd02b7d05499a818042383aa8e83",
+    "url": "/test-git-pages/dev/static/js/2.3dd218b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ee8a3c4087589bf1544",
+    "url": "/test-git-pages/dev/static/js/main.e42bae49.chunk.js"
+  },
+  {
+    "revision": "f156c63c63e633a1065a",
+    "url": "/test-git-pages/dev/static/js/runtime-main.f68f4f64.js"
+  }
+]);
