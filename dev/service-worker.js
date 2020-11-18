fb09bd19edc0421f2006bc293d0903c0206b84e3
Christie Lincoln <clincoln@ucsd.edu>
2020-11-17 22:30:48 -0800

    ui updates

--- dev/service-worker.js
+++ dev/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/test-git-pages/dev/precache-manifest.a6737ea177832aae31aeee2ed3e62872.js"
+  "/test-git-pages/dev/precache-manifest.67c515d2ba0c23ceed4350e1ecf8f187.js"
 );
 
 self.addEventListener('message', (event) => {
